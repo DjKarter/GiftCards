b4a6d6a2b4b159054dc075358655540eff1cd101
Dmitrii <startzew.dima@mail.ru>
2024-11-19 19:44:23 +0300

    GH-pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const webpack = require("webpack");
 const path = require("path");
-const ESLintWebpackPlugin = require('eslint-webpack-plugin');
+
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
@@ -32,12 +32,6 @@ module.exports = {
         extensions: ["*", ".js", ".jsx", ".scss"],
     },
     plugins: [
-        new ESLintWebpackPlugin({
-            overrideConfigFile: path.resolve(__dirname, 'eslint.config.mjs'),
-            extensions: ['js', 'jsx'],
-            failOnError: false,
-            emitWarning: true,
-        }),
         new CleanWebpackPlugin(),
         new webpack.HotModuleReplacementPlugin(),
         new HtmlWebpackPlugin({
